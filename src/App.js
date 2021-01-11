9155c849d87169752e61881db8d0680776e0821a
Vijayavengadam <vijayavengadam0403@gmail.com>
2021-01-11 19:57:57 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{ useState } from 'react';
+import './App.css';
+import Login from './components/login';
+
+function App() {
+
+  const[token,setToken] = useState('');
+
+  const userLogin =  (tok) =>{
+    setToken(tok);
+    console.log(tok);
+    console.log(token);
+
+  }
+  
+
+  return (
+    <div className="App">
+      <Login userLogin={userLogin}/>
+    </div>
+  );
+}
+
+export default App;
